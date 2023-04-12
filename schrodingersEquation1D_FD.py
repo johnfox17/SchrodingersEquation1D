7c84d5051ee805d3df972cd1e37675432d433c73
johnfox17 <johnfox17@gmail.com>
2023-04-11 21:10:00 -0700

    Added capability for finding Eigen Vectors and Eigen Values

--- schrodingersEquation1D_FD.py
+++ schrodingersEquation1D_FD.py
@@ -7,16 +7,21 @@ from scipy.sparse import spdiags
 from scipy.sparse.linalg import spsolve
 from scipy.sparse.linalg import inv
 from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import eigs
+
 
 #Defining constants
-L = 30
-dx = 1
+L = 10
+dx = 0.1
 k = 1/(2*dx**2)
-w=1
 n = 2 #Ground State
-En = w*(n+0.5)
 xCoords = np.arange(-L/2,L/2+dx, dx) #create the discrete x and y grids
 numNodes = len(xCoords)
+
+#np.savetxt("/home/doctajfox/Documents/Thesis_Research/SolidStatePhysics/SchrodingersEquation1D/data/K.csv", K, delimiter=",")
+#np.savetxt("/home/doctajfox/Documents/Thesis_Research/SolidStatePhysics/SchrodingersEquation1D/data/v.csv", V, delimiter=",")
+#np.savetxt("C:\\Users\\johnf\\Documents\\Thesis\\SchrodingersEquation1D\\data\\K.csv", K, delimiter = ",")
+#np.savetxt("C:\\Users\\johnf\\Documents\\Thesis\\SchrodingersEquation1D\\data\\V.csv", V, delimiter = ",")
 
 def calculatePotentialEnergy():
     V = 0.5*w**2*xCoords**2
@@ -24,32 +29,23 @@ def calculatePotentialEnergy():
 
 def createKMatrix(V):
     data0 = np.array([(numNodes-3)*[-k]])
-    data1 = np.array([(numNodes-2)*[2*k-En]])
+    data1 = np.array([(numNodes-2)*[2*k]])
     data2 = np.array([(numNodes-2)*[-k]])
     K = spdiags(data0, -1, numNodes-2, numNodes-2 ).toarray()\
             +spdiags(data1, 0, numNodes-2, numNodes-2 ).toarray()\
             +spdiags(data2, 1, numNodes-2, numNodes-2 ).toarray()\
             +spdiags(V[1:numNodes-1], 0, numNodes-2, numNodes-2 ).toarray()
-            #+spdiags(data2, 1, numNodes-2, numNodes-2 ).toarray()
-    
-    np.savetxt("/home/doctajfox/Documents/Thesis_Research/SolidStatePhysics/SchrodingersEquation1D/data/K.csv", K, delimiter=",")
-    np.savetxt("/home/doctajfox/Documents/Thesis_Research/SolidStatePhysics/SchrodingersEquation1D/data/v.csv", V, delimiter=",")
-    print(K)
-    return csc_matrix(K)
+    return K
 
 def main():
     V = calculatePotentialEnergy() 
-    print(np.shape(V[1:29]))
     K = createKMatrix(V)
+    En, Phi = eigs(K) 
 
-    phi = spsolve(K,np.zeros(numNodes-2))
-
-    b = np.zeros(numNodes-2) 
-    phi = spsolve(K,b)
 
     #a = input('').split(" ")[0]
     fig, ax = plt.subplots()
-    ax.plot(phi)
+    ax.plot(np.absolute(Phi[:,n]),marker='o')
     ax.grid()
     plt.show()
 
